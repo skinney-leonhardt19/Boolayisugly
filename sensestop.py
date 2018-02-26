b86a21a259fd727bd60a09501fe998ccc9d11d18
skinney-leonhardt19 <33100407+skinney-leonhardt19@users.noreply.github.com>
2018-02-26 09:40:19 -0600

    Update sensestop.py

--- sensestop.py
+++ sensestop.py
@@ -3,12 +3,11 @@ RPL.RoboPiInit("/dev/ttyAMA0",115200)
 
 RPL.pinMode(17, RPL.INPUT)
 
-While True:
-    if RPL.digitalRead(17) == 0:
-        RPL.servoWrite(0, 0)
-        RPL.servoWrite(1, 0)
-        break
-    
-    if RPL.digitalRead(17) == 1:
-        RPL.servoWrite(0, 2000)
-        RPL.servoWrite(1, 1000)
+While RPL.digitalRead(16) == 0:
+    RPL.servoWrite(0, 0)
+    RPL.servoWrite(1, 0)
+    break
+While RPL.digitalRead(17) == 1:
+    RPL.servoWrite(0, 2000)
+    RPL.servoWrite(1, 1000)
+    break
