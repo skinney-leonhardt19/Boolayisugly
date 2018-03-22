d0555fcfe2ba03161b607fe6be9bee507301db17
skinney-leonhardt19 <33100407+skinney-leonhardt19@users.noreply.github.com>
2018-03-22 09:20:16 -0500

    Update wallstop.py

--- wallstop.py
+++ wallstop.py
@@ -1,4 +1,26 @@
 import RoboPiLib as RPL
 RPL.RoboPiInit("/dev/ttyAMA0",115200)
 
+import sys, tty, termios, signal
+
+import time
+
 RPL.pinMode(17, RPL.input)
+
+motorR == 1
+motorL == 0
+
+while True:
+    sensorRead == RPL.digitalRead(17)
+ global move == time.time 
+    if sensorRead == 1:
+        RPL.servoWrite(0, 1000)
+        RPL.servoWrite(1, 2000)
+    if sensorRead == 0:
+        RPL.servoWrite(0, 1490)
+        RPL.servoWrite(1, 1510)
+    if move == time.time + 3:
+        RPL.servoWrite(0, 0)
+        RPL.servoWrite(1, 0)
+
+    
