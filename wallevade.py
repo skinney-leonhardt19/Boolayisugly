543def35e0c98aae4838e251adb65a3b6aceeb71
skinney-leonhardt19 <33100407+skinney-leonhardt19@users.noreply.github.com>
2018-04-12 09:26:34 -0500

    Create wallevade.py

---
+++ wallevade.py
@@ -0,0 +1,20 @@
+import RoboPiLib as RPL
+RPL.RoboPiInit("/dev/ttyAMA0",115200)
+
+import sys, tty, termios, signal
+
+import time
+
+RPL.pinMode(17, RPL.input)
+
+motorR == 1
+motorL == 0
+
+while True:
+    sensorRead == RPL.digitalRead(17) 
+    if sensorRead == 1:
+        RPL.servoWrite(0, 1000)
+        RPL.servoWrite(1, 2000)
+    if sensorRead == 0:
+        RPL.servoWrite(0, 1500)
+        RPL.servoWrite(1, 2000)
