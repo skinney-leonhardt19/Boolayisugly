705be8d2417ad8e95ced56782927d3c7d82089f4
skinney-leonhardt19 <33100407+skinney-leonhardt19@users.noreply.github.com>
2018-04-19 09:15:29 -0500

    Update wallevade.py

--- wallevade.py
+++ wallevade.py
@@ -12,5 +12,5 @@ while True:
         RPL.servoWrite(0, 1000)
         RPL.servoWrite(1, 2000)
     if RPL.digitalRead(17) == 0:
-        RPL.servoWrite(0, 1500)
+        RPL.servoWrite(0, 2000)
         RPL.servoWrite(1, 2000)
