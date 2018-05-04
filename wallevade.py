535b15e6f8797266fdbf0505d7c7eacdd9be72fc
skinney-leonhardt19 <33100407+skinney-leonhardt19@users.noreply.github.com>
2018-05-04 06:52:10 -0500

    Update wallevade.py

--- wallevade.py
+++ wallevade.py
@@ -7,10 +7,10 @@ import time
 
 RPL.pinMode(16, RPL.INPUT)
 
-while True: 
-    if RPL.digitalRead(16) == 1:
+
+    while RPL.digitalRead(16) == 1:
         RPL.servoWrite(0, 2000)
         RPL.servoWrite(1, 1000)
-    if RPL.digitalRead(16) == 0:
+    while RPL.digitalRead(16) == 0:
         RPL.servoWrite(0, 1490)
         RPL.servoWrite(1, 2000)
